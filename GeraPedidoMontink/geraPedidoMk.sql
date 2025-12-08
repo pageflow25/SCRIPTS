314ad458ca2b98487812ef73a83152eb4010b086
arthuzao <arthur88727771@gmail.com>
2025-12-08 17:33:15 -0300

    Refactor code structure for improved readability and maintainability

---
+++ GeraPedidoMontink/geraPedidoMk.sql
@@ -0,0 +1,14 @@
+SELECT 
+    p.id, 
+    p.etiqueta_envio, 
+    pp.nome ,
+    pp.arquivo_pdf,
+    pp.design_capa_frente,
+    pp.design_capa_verso,
+    pp.mockup_capa_frente,
+    pp.mockup_capa_costas, 
+    pp.descricao 
+FROM pedidos p
+INNER JOIN pedido_produtos pp 
+    ON p.id = pp.pedido_id
+WHERE p.id IN (919, 920, 921);
